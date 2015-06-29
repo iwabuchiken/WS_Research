e45fc72dd518a2004e8f8e709bb00a39b941d14c
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-06-29 17:21:21 +0900

    R.160
    s-1#1
    ---
    1. up to => makefile: obj file
    
    	==> done

---
+++ R.150-199/R.160_openjpeg/s-1/test.c
@@ -0,0 +1,18 @@
+/*
+ * test.c
+ *
+ *  Created on: 2015/06/29
+ *      Author: kbuchi
+ */
+
+#include <stdio.h>
+
+//#include <zlib.h>
+//#include <png.h>
+
+int main() {
+
+	printf("[%s:%d] done\n", __FILE__, __LINE__);
+
+
+}
