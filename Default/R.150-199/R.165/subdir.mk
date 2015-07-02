093008eba7998391732e5de21a267d1bd3b30b8c
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-07-03 07:23:29 +0900

    R.165
    s-1
    ---
    1. new main.c file
    
        ==> started

--- Default/R.150-199/R.165/subdir.mk
+++ Default/R.150-199/R.165/subdir.mk
@@ -3,22 +3,18 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
-O_SRCS += \
-../R.150-199/R.160_openjpeg/s-1/convert.o \
-../R.150-199/R.160_openjpeg/s-1/main.o 
-
 C_SRCS += \
-../R.150-199/R.160_openjpeg/s-1/convert.c 
+../R.150-199/R.165/main.c 
 
 OBJS += \
-./R.150-199/R.160_openjpeg/s-1/convert.o 
+./R.150-199/R.165/main.o 
 
 C_DEPS += \
-./R.150-199/R.160_openjpeg/s-1/convert.d 
+./R.150-199/R.165/main.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-R.150-199/R.160_openjpeg/s-1/%.o: ../R.150-199/R.160_openjpeg/s-1/%.c
+R.150-199/R.165/%.o: ../R.150-199/R.165/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
 	gcc -I"C:\MinGW\mingw32\i686-w64-mingw32\include" -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
--- Default/objects.mk
+++ Default/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lopenjp2
+LIBS :=
 
