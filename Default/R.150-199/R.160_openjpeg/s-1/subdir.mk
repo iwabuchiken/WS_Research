fbceaf88d95d6e2a9fe73b803b65aa5087258705
iwabuchiken <iwabuchi.k.2010@gmail.com>
2015-07-03 13:42:01 +0900

    R.165(163)
    s-1
    ---
    1. install Qt
    2. try using Qt
    
        ==> done

--- Default/R.150-199/R.160_openjpeg/s-1/subdir.mk
+++ Default/R.150-199/R.160_openjpeg/s-1/subdir.mk
@@ -7,22 +7,6 @@ O_SRCS += \
 ../R.150-199/R.160_openjpeg/s-1/convert.o \
 ../R.150-199/R.160_openjpeg/s-1/main.o 
 
-C_SRCS += \
-../R.150-199/R.160_openjpeg/s-1/convert.c 
-
-OBJS += \
-./R.150-199/R.160_openjpeg/s-1/convert.o 
-
-C_DEPS += \
-./R.150-199/R.160_openjpeg/s-1/convert.d 
-
 
 # Each subdirectory must supply rules for building sources it contributes
-R.150-199/R.160_openjpeg/s-1/%.o: ../R.150-199/R.160_openjpeg/s-1/%.c
-	@echo 'Building file: $<'
-	@echo 'Invoking: GCC C Compiler'
-	gcc -I"C:\MinGW\mingw32\i686-w64-mingw32\include" -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
 
